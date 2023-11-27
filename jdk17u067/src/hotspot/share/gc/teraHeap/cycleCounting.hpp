0ccde6e40aea73cf552c4fe8988cde173d220d0e
Periklis Papoutsis <perpap@ics.forth.gr>
2023-11-27 18:58:34 +0200

    Cross-platform cpu clock cycle counting.

---
+++ jdk17u067/src/hotspot/share/gc/teraHeap/cycleCounting.hpp
@@ -0,0 +1,85 @@
+/*
+ * cycle_counting.hpp
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: perpap
+ */
+
+#ifndef _SHARE_GC_TERAHEAP_CYCLE_COUNTING_HPP_
+#define _SHARE_GC_TERAHEAP_CYCLE_COUNTING_HPP_
+
+#ifndef __GNUC__
+# error "Works only on GCC"
+#endif
+
+#include <stdint.h>
+
+#if defined(__x86_64__)
+
+static inline uint64_t get_cycles()
+{
+    uint32_t low, high;
+    asm volatile("rdtsc" : "=a" (low), "=d" (high));
+    return ((uint64_t)high << 32) | low;
+}
+
+#elif defined(__arm__) || defined(__aarch64__)
+
+static inline uint64_t get_cycles()
+{
+    uint64_t val;
+    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (val));
+    return val;
+}
+
+#else
+#error "Unsupported architecture, x86-64 and AArch64 only."
+#endif
+
+#if defined(_WIN32)
+#include <windows.h>
+
+uint64_t get_cpu_frequency() {
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+    return frequency.QuadPart;
+}
+
+#elif defined(__linux__)
+#include <stdio.h>
+
+uint64_t get_cpu_frequency() {
+    FILE *fp = fopen("/proc/cpuinfo", "r");
+    if (!fp) return 0;
+
+    char buffer[1024];
+    uint64_t frequency = 0;
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        if (sscanf(buffer, "cpu MHz : %llu", &frequency)) {
+            frequency *= 1000000; // Convert MHz to Hz
+            break;
+        }
+    }
+
+    fclose(fp);
+    return frequency;
+}
+
+#else
+#error "Unsupported platform"
+#endif
+
+uint64_t get_cycles_per_second() {
+    static uint64_t cpu_frequency = 0;
+    if (cpu_frequency == 0) {
+        cpu_frequency = get_cpu_frequency();
+        if (cpu_frequency == 0) {
+            // Fallback to a default value if detection fails
+            cpu_frequency = 2000000000; // Example: 2 GHz
+        }
+    }
+    return cpu_frequency;
+}
+
+#endif /* _SHARE_GC_TERAHEAP_CYCLE_COUNTING_HPP_ */
--- jdk17u067/src/hotspot/share/gc/teraHeap/teraTimers.hpp
+++ jdk17u067/src/hotspot/share/gc/teraHeap/teraTimers.hpp
@@ -5,8 +5,12 @@
 #include "gc/teraHeap/teraDynamicResizingPolicy.hpp"
 #include <sys/time.h>
 
+#define rdtsc get_cycles
+
 class TeraTimers: public CHeapObj<mtInternal> {
 private:
+  static const uint64_t CYCLES_PER_SECOND;
+
   uint64_t h2_scavenge_start_time;
   uint64_t h2_scavenge_end_time;
 
@@ -50,7 +54,7 @@ private:
   uint64_t malloc_end_time;
   double malloc_time_per_gc;
 
-  uint64_t rdtsc();
+  //uint64_t rdtsc(); //FIXME
   void print_ellapsed_time(uint64_t start_time, uint64_t end_time, char* msg);
 
 public:
