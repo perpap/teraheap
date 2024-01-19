4045dc15a0942fc02bf00c5985aaf0f6c5fd1fdc
Periklis Papoutsis <perpap@ics.forth.gr>
2024-01-19 17:00:26 +0200

    implement a post-write barrier for aarch64 port of Teraheap

--- jdk17u067/src/hotspot/cpu/aarch64/gc/shared/cardTableBarrierSetAssembler_aarch64.cpp
+++ jdk17u067/src/hotspot/cpu/aarch64/gc/shared/cardTableBarrierSetAssembler_aarch64.cpp
@@ -33,119 +33,6 @@
 
 #define __ masm->
 
-void CardTableBarrierSetAssembler::store_check(MacroAssembler* masm, Register obj, Address dst) {
-
-  BarrierSet* bs = BarrierSet::barrier_set();
-  assert(bs->kind() == BarrierSet::CardTableBarrierSet, "Wrong barrier set kind");
-
-  __ lsr(obj, obj, CardTable::card_shift);
-
-  assert(CardTable::dirty_card_val() == 0, "must be");
-
-  __ load_byte_map_base(rscratch1);
-
-  if (UseCondCardMark) {
-    Label L_already_dirty;
-    __ ldrb(rscratch2,  Address(obj, rscratch1));
-    __ cbz(rscratch2, L_already_dirty);
-    __ strb(zr, Address(obj, rscratch1));
-    __ bind(L_already_dirty);
-  } else {
-    __ strb(zr, Address(obj, rscratch1));
-  }
-}
-
-#if 1//perpap
-#ifdef TERA_INTERPRETER
-void CardTableBarrierSetAssembler::store_check_part1(MacroAssembler* masm, Register obj) {
-    __ shrptr(obj, CardTable::card_shift);
-}
-
-void CardTableBarrierSetAssembler::h2_store_check_part1(MacroAssembler* masm, Register obj) {
-    __ shrptr(obj, CardTable::th_card_shift);
-}
-
-void CardTableBarrierSetAssembler::store_check_part2(MacroAssembler* masm, Register obj) {
-  // Does a store check for the oop in register obj. The content of
-  // register obj is destroyed afterwards.
-  BarrierSet* bs = BarrierSet::barrier_set();
-
-  CardTableBarrierSet* ctbs = barrier_set_cast<CardTableBarrierSet>(bs);
-  CardTable* ct = ctbs->card_table();
-  Address card_addr;
-
-  // The calculation for byte_map_base is as follows:
-  // byte_map_base = _byte_map - (uintptr_t(low_bound) >> card_shift);
-  // So this essentially converts an address to a displacement and it will
-  // never need to be relocated. On 64bit however the value may be too
-  // large for a 32bit displacement.
-  intptr_t byte_map_base = (intptr_t)ct->byte_map_base();
-  if (__ is_simm32(byte_map_base)) {
-    card_addr = Address(noreg, obj, Address::times_1, byte_map_base);
-  } else {
-    // By doing it as an ExternalAddress 'byte_map_base' could be converted to a rip-relative
-    // displacement and done in a single instruction given favorable mapping and a
-    // smarter version of as_Address. However, 'ExternalAddress' generates a relocation
-    // entry and that entry is not properly handled by the relocation code.
-    AddressLiteral cardtable((address)byte_map_base, relocInfo::none);
-    Address index(noreg, obj, Address::times_1);
-    card_addr = __ as_Address(ArrayAddress(cardtable, index));
-  }
-
-  int dirty = CardTable::dirty_card_val();
-  if (UseCondCardMark) {
-    Label L_already_dirty;
-    __ cmpb(card_addr, dirty);
-    __ jcc(Assembler::equal, L_already_dirty);
-    __ movb(card_addr, dirty);
-    __ bind(L_already_dirty);
-  } else {
-    __ movb(card_addr, dirty);
-  }
-}
-
-void CardTableBarrierSetAssembler::h2_store_check_part2(MacroAssembler* masm, Register obj) {
-  // Does a store check for the oop in register obj. The content of
-  // register obj is destroyed afterwards.
-  BarrierSet* bs = BarrierSet::barrier_set();
-
-  CardTableBarrierSet* ctbs = barrier_set_cast<CardTableBarrierSet>(bs);
-  CardTable* ct = ctbs->card_table();
-  Address card_addr;
-
-  // The calculation for byte_map_base is as follows:
-  // byte_map_base = _byte_map - (uintptr_t(low_bound) >> card_shift);
-  // So this essentially converts an address to a displacement and it will
-  // never need to be relocated. On 64bit however the value may be too
-  // large for a 32bit displacement.
-  intptr_t th_byte_map_base = (intptr_t)ct->th_byte_map_base();
-  if (__ is_simm32(th_byte_map_base)) {
-    card_addr = Address(noreg, obj, Address::times_1, th_byte_map_base);
-  } else {
-    // By doing it as an ExternalAddress 'byte_map_base' could be converted to a rip-relative
-    // displacement and done in a single instruction given favorable mapping and a
-    // smarter version of as_Address. However, 'ExternalAddress' generates a relocation
-    // entry and that entry is not properly handled by the relocation code.
-    AddressLiteral cardtable((address)th_byte_map_base, relocInfo::none);
-    Address index(noreg, obj, Address::times_1);
-    card_addr = __ as_Address(ArrayAddress(cardtable, index));
-  }
-
-  int dirty = CardTable::dirty_card_val();
-  if (UseCondCardMark) {
-    Label L_already_dirty;
-    __ cmpb(card_addr, dirty);
-    __ jcc(Assembler::equal, L_already_dirty);
-    __ movb(card_addr, dirty);
-    __ bind(L_already_dirty);
-  } else {
-    __ movb(card_addr, dirty);
-  }
-}
-
-#endif // TERA_INTERPRETER
-#endif//perpap
-
 void CardTableBarrierSetAssembler::gen_write_ref_array_post_barrier(MacroAssembler* masm, DecoratorSet decorators,
                                                                     Register start, Register count, Register scratch, RegSet saved_regs) {
   Label L_loop, L_done;
@@ -167,6 +54,60 @@ void CardTableBarrierSetAssembler::gen_write_ref_array_post_barrier(MacroAssembl
   __ br(Assembler::GE, L_loop);
   __ bind(L_done);
 }
+
+#ifdef TERA_INTERPRETER
+void CardTableBarrierSetAssembler::store_check(MacroAssembler* masm, Register obj, Address dst) {
+  Label L_in_h2;
+	Label L_Done;
+
+  if (EnableTeraHeap) {
+    AddressLiteral h2_start_addr((address)Universe::teraHeap()->h2_start_addr(), relocInfo::none);
+    // Push the teraCache address in r11
+    __ lea(r11, h2_start_addr);
+
+    __ cmpptr(obj, r11);
+
+		__ jcc(Assembler::greaterEqual, L_in_h2);
+
+    // Does a store check for the oop in register obj. The content of
+    // register obj is destroyed afterwards.
+		store_check_part1(masm, obj);
+		store_check_part2(masm, obj);
+		__ jmp(L_Done);
+
+    __ BIND(L_in_h2);
+    h2_store_check_part1(masm, obj);
+		h2_store_check_part2(masm, obj);
+
+		__ BIND(L_Done);
+  } else {
+    store_check_part1(masm, obj);
+    store_check_part2(masm, obj);
+  }
+}
+#else
+void CardTableBarrierSetAssembler::store_check(MacroAssembler* masm, Register obj, Address dst) {
+
+  BarrierSet* bs = BarrierSet::barrier_set();
+  assert(bs->kind() == BarrierSet::CardTableBarrierSet, "Wrong barrier set kind");
+
+  __ lsr(obj, obj, CardTable::card_shift);
+
+  assert(CardTable::dirty_card_val() == 0, "must be");
+
+  __ load_byte_map_base(rscratch1);
+
+  if (UseCondCardMark) {
+    Label L_already_dirty;
+    __ ldrb(rscratch2,  Address(obj, rscratch1));
+    __ cbz(rscratch2, L_already_dirty);
+    __ strb(zr, Address(obj, rscratch1));
+    __ bind(L_already_dirty);
+  } else {
+    __ strb(zr, Address(obj, rscratch1));
+  }
+}
+#endif //TERA_INTERPRETER
 
 void CardTableBarrierSetAssembler::oop_store_at(MacroAssembler* masm, DecoratorSet decorators, BasicType type,
                                                 Address dst, Register val, Register tmp1, Register tmp2) {
