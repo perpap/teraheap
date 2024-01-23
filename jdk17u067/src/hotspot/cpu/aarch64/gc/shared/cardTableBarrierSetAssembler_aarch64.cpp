a39c754ce62abf39957b69b207634c80aea09aec
perpap <perpap@pop-os.localdomain>
2024-01-23 21:16:31 +0200

    Fixed missing include header teraHeap.hpp

--- jdk17u067/src/hotspot/cpu/aarch64/gc/shared/cardTableBarrierSetAssembler_aarch64.cpp
+++ jdk17u067/src/hotspot/cpu/aarch64/gc/shared/cardTableBarrierSetAssembler_aarch64.cpp
@@ -29,6 +29,7 @@
 #include "gc/shared/cardTableBarrierSet.hpp"
 #include "gc/shared/cardTableBarrierSetAssembler.hpp"
 #include "gc/shared/gc_globals.hpp"
+#include "gc/teraHeap/teraHeap.hpp"
 #include "interpreter/interp_masm.hpp"
 
 #define __ masm->
