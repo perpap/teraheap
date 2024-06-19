b7b2090ca9faa4d5a9e028c58129bedbb91ab25d
Periklis Papoutsis <perpap@ics.forth.gr>
2024-06-20 00:57:39 +0200

    initialize H2 after H1

--- jdk17u067/src/hotspot/share/memory/universe.cpp
+++ jdk17u067/src/hotspot/share/memory/universe.cpp
@@ -804,7 +804,7 @@ jint Universe::initialize_heap() {
   _collectedHeap = GCConfig::arguments()->create_heap();
 
   log_info(gc)("Using %s", _collectedHeap->name());
-#if 1//perpap
+#if 0//perpap
   if (EnableTeraHeap) {
     _teraHeap = new TeraHeap(nullptr);//new TeraHeap();
     log_info(gc)("Initialize Teraheap");
