0fd4d2da5a396f2eec38cc24e9e092c11a495afb
Jack Kolokasis <kolokasis@ics.forth.gr>
2024-08-07 14:33:02 +0300

    [clean] Remove unused scripts in java tests

--- tests/java/compile.sh
+++ tests/java/compile.sh
@@ -14,4 +14,41 @@ export PATH=${PROJECT_DIR}/tera_malloc/include/:$PATH
 export C_INCLUDE_PATH=${PROJECT_DIR}/tera_malloc/include/:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=${PROJECT_DIR}/tera_malloc/include/:$CPLUS_INCLUDE_PATH
 
-make all
+# Usage function to display script usage
+usage() {
+  echo "Usage: $0 -r | -d"
+  echo "  -r    Run in release mode"
+  echo "  -d    Run in debug mode"
+  exit 1
+}
+
+# Check if an argument is provided
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+# Parse the argument
+while getopts "rd" opt; do
+  case ${opt} in
+    r)
+      MODE="release"
+      ;;
+    d)
+      MODE="debug"
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+# Execute based on the mode
+if [ "$MODE" == "release" ]; then
+  echo "Compile with release mode..."
+  make -f Makefile_release all
+elif [ "$MODE" == "debug" ]; then
+  echo "Running in debug mode..."
+  make -f Makefile_fastdebug all
+else
+  usage
+fi
