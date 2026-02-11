b21f3673512046ffc4fba4d9a2980be002c07ff6
Periklis Papoutsis <perpap@ics.forth.gr>
2026-02-11 13:12:47 +0000

    [aarch64]Fix compilation error due to unknown type for 'uint64_t'

--- tera_malloc/include/tera_allocator.h
+++ tera_malloc/include/tera_allocator.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 
-#if defined (__ia64__) || defined (__x86_64__)
+#if defined (__ia64__) || defined (__x86_64__) || defined (__aarch64__)
 #define INT_PTR unsigned long
 
 #include <inttypes.h>
