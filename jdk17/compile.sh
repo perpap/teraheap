fc825691e6c60efc32262c779ca37e9e5deec737
Periklis Papoutsis <perpap@ics.forth.gr>
2026-05-06 00:28:11 +0000

    update help message

--- jdk17/compile.sh
+++ jdk17/compile.sh
@@ -68,12 +68,12 @@ function usage() {
   echo
   echo "   Examples:"
   echo
-  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ -i \"release\"                            Configure and build a \"release\" image usigne ."
-  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ -i \"optimized\" -s \"internal\"          Configure and build an \"optimized\" image with \"internal\" debug symbols."
-  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ --image \"fastdebug\"                     Configure and build a \"fastdebug\" image."
-  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ -g 7.4.0 -i \"release\"                   Configure and build a \"release\" image using gcc-7.4.0"
-  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ -g 13.2.0 -i \"release\"                  Configure and build a \"release\" image using gcc-13.2.0"
-  echo "  ./compile.sh -t x86_64 -m \"release\"                                                                         Relink a \"release\" image without running configure."
+  echo "  ./compile.sh -b /archive/users/perpap/openjdk/aarch64/jdk-16.0.2+7/ -i \"release\" -s \"internal\"                 Configure and build a \"release\" image with \"internal\" debug symbols for the aarch64 platform. "
+  echo "  ./compile.sh -t aarch64 -b /archive/users/perpap/openjdk/aarch64/jdk-16.0.2+7/ -i \"release\" -s \"internal\"      Configure and build a \"release\" image with \"internal\" debug symbols for the aarch64 platform. "
+  echo "  ./compile.sh -b /archive/users/perpap/openjdk/aarch64/jdk-16.0.2+7/ -i \"release\" -s \"internal\" -m \"release\"  Incremental build for a \"release\" image with \"internal\" debug symbols for the aarch64 platform. "
+  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ -i \"release\"                                 Configure and build a \"release\" image for the x86_64 platform."
+  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ -i \"optimized\" -s \"internal\"               Configure and build an \"optimized\" image with \"internal\" debug symbols for the x86_64 platform."
+  echo "  ./compile.sh -t x86_64 -b /spare/perpap/openjdk/jdk16/jdk-16.0.2+7/ --image \"fastdebug\"                          Configure and build a \"fastdebug\" image for the x86_64 platform."
   return 0 2>/dev/null || exit 0
 }
 
